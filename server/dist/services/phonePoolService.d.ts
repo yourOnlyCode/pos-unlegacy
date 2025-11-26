965f1865d186af02c2d67f0e5ef02f0acd8839e0
Matthew Mullin <matthew.mullin@charter.com>
2025-11-26 08:47:55 -0500

    deleted dist folder

--- server/dist/services/phonePoolService.d.ts
+++
@@ -1,9 +0,0 @@
-export declare function assignPhoneNumber(businessId: string): Promise<string | null>;
-export declare function releasePhoneNumber(businessId: string): Promise<boolean>;
-export declare function getPurchasedNumbers(): Array<{
-    number: string;
-    businessId: string;
-    purchasedAt: Date;
-}>;
-export declare function getNumberByBusiness(businessId: string): string | null;
-//# sourceMappingURL=phonePoolService.d.ts.map
